5ac157fbdb9c174bff3ae2085ef746431397604c
valen rofail <valengeorge14@gmail.com>
2025-11-11 09:01:31 +0200

    Add Person class with name and id attributes

--- Person.h
+++ Person.h
@@ -6,7 +6,18 @@ using namespace std;
 
 //#write Person class here
 
+private:
+string name;
+int id;
 
+public:
+Person(string n, int d){
+  name = n;
+  id = d;
+}
+void display(){
+  cout << "Name:" << name << "ID:" << id << endl;
+}
 
 
 
