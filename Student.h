f584f47ed92d90baa17b9af5c76a7731ee109f3e
valen rofail <valengeorge14@gmail.com>
2025-11-11 09:02:24 +0200

    Update Student class with yearLevel and major
    Fix constructor and display method in Student class.

--- Student.h
+++ Student.h
@@ -6,10 +6,19 @@
 #using namespace std;
 //#write student class here
 
+private:
+    int yearLevel;
+    string major;
 
+public:
+    Student(string n, int d, int y, string m): Person(n, d){
+        yearLlevel = y;
+        major = m;
+    }
+    void display(){
+        cout << "Name: " << name << "ID: " << id << "Year level: " << y << "Major: " << major << endl;
 
-
-
+}
 
 
 
